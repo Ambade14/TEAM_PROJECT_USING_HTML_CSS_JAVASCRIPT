c9a361653f4c7aadc09ce17f142521a496a58789
Ambade14 <128669029+Ambade14@users.noreply.github.com>
2023-08-27 16:02:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+
+// University data
+const universities = [
+    "Combridge University",
+    "Harvard University",
+    "California University",
+    "ETH Zurich University",
+    "Max Plan University",
+    "Edienberg University",
+    "Johns Hopins University",
+    "Midgan state University",
+    "Illioois University",
+    "Humbold University"
+  ];
+  
+  // Mentor data
+  const mentors = [
+    "Shubham",
+    "Mangesh",
+    "Ankit",
+    "Gaurav",
+    "Shushant",
+    "Aniket",
+    "Viru",
+    "Vikas",
+    "Kunal",
+    "Vilas"
+  ];
+  
+  // Advisor data
+  const advisors = [
+    "Dr Omkar",
+    "Akhil Tripathi",
+    "Vivek Bindra"
+  ];
+
+  const advisor = [
+    "Dr Omkar",
+    "Akhil Tripathi",
+    "Vivek Bindra"
+  ]
+  
+  // Update University information
+ const universityIndex = 4; // Index of the desired university from the array
+  document.getElementById('Cambridge university').textContent = universities[universityIndex];
+  // Update Mentor information
+  const mentorIndex = 1; // Index of the desired mentor from the array
+  document.getElementById('Shubham').textContent = mentors[mentorIndex];
+  // Update Advisor information
+  const advisorIndex = 0; // Index of the desired advisor from the array
+  document.getElementById('Dr. Omkar').textContent = advisors[advisorIndex];
+
+ // const universityIndex = 4;
+  document.getElementById('Harvard University').textContent = universities[universityIndex];
+  //const mentorIndex = 1;
+  document.getElementById('Mangesh').textContent = mentors[mentorIndex];
+  //const advisorIndex = 0; // Index of the desired advisor from the array
+  document.getElementById('Akhil Tripathi').textContent = advisors[advisorIndex];
+  
+   // const universityIndex = 4;
+   document.getElementById('California University').textContent = universities[universityIndex];
+   //const mentorIndex = 1;
+   document.getElementById('Ankit').textContent = mentors[mentorIndex];
+   //const advisorIndex = 0; // Index of the desired advisor from the array
+   document.getElementById('Akhil Tripathi').textContent = advisors[advisorIndex];
+
+      // const universityIndex = 4;
+      document.getElementById('California University').textContent = universities[universityIndex];
+      //const mentorIndex = 1;
+      document.getElementById('Ankit').textContent = mentors[mentorIndex];
+      //const advisorIndex = 0; // Index of the desired advisor from the array
+      document.getElementById('Akhil Tripathi').textContent = advisors[advisorIndex];
